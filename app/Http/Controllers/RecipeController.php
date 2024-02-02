0d0baef25685b86929903e72b32da651712f3385
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-02-02 03:39:24 +0100

    creating alsomst logic for 99of crud of recipe

--- app/Http/Controllers/RecipeController.php
+++ app/Http/Controllers/RecipeController.php
@@ -23,4 +23,56 @@ class RecipeController extends Controller
     {
         return view('add_recipe');
     }
+
+    /**
+     * store a newly created recipe in storage
+     */
+    public function store(Request $request)
+    {
+        $incomingFields = $request->validate([
+            'title' => "required",
+            'ingredients' => "required",
+            'recipe' => "required",
+        ]);
+        $imageName = time().'.'.$request->file('picture')->extenstion();
+        $request->picture->move(public_path("storage"), $imageName);
+        $incomingFields["picture"] = $imageName;
+
+        Recipe::create($incomingFields);
+        return redirect("/");
+    }
+
+/**
+ * Show the form for editing the specified resource.
+ */
+    public function edit(Recipe $recipe)
+    {
+        return view("update_recipe",  ['recipe' => $recipe]);
+    }
+
+/**
+ * Update the specified recipe in storage.
+ */
+    public function update(Request $request, Recipe $recipe)
+    {
+        $incomingFields = $request->validate([
+            'title' => 'required',
+            'recipe' => 'required',
+            'ingredients' => 'required',
+        ]);
+        $imageName = time() . '.' . $request->file('picture')->extension();
+        $request->picture->move(public_path("storage"), $imageName);
+        $incomingFields["picture"] = $imageName;
+
+        $recipe->update($incomingFields);
+        return redirect("/");
+    }
+
+/**
+ * Remove the specified recipe from storage.
+ */
+    public function destroy(Recipe $recipe){$recipe->delete();
+        return redirect('/');}
+
+
 }
