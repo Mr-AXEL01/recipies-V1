71d550d2d2df72917b3552150ecf133d8dc6e5f7
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-02-02 03:09:06 +0100

    creating indexmethod in the recipeController for displaying all therecipes

--- app/Http/Controllers/RecipeController.php
+++ app/Http/Controllers/RecipeController.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Recipe;
 use Illuminate\Http\Request;
 
 class RecipeController extends Controller
 {
-    //
+    /**
+     * Displaying a listing of the recipes
+     */
+    public function index()
+    {
+        $recipes = Recipe::all();
+        return view('home',compact(recipes));
+    }
 }
